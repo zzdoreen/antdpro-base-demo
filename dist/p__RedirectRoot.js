5847f04d85dcca80b31c9a5f63300fe0c8215222
zzdoreen <zz1061831341@qq.com>
2023-12-11 22:29:31 +0800

    feat(electron): 相关配置

--- dist/p__RedirectRoot.js
+++ dist/p__RedirectRoot.js
@@ -1,1 +1,1 @@
-(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[401],{79935:function(d,t,n){"use strict";n.r(t),n.d(t,{default:function(){return o}});var u=n(51615),s=n(85893);function o(){return(0,s.jsx)(u.l_,{to:"/list"})}}}]);
+(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[401],{50376:function(d,t,n){"use strict";n.r(t),n.d(t,{default:function(){return o}});var u=n(73630),s=n(37712);function o(){return(0,s.jsx)(u.l_,{to:"/list"})}}}]);
---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            devTools: true,
+            nodeIntegration: true,
+            webSecurity: false
+        }
+    })
+    win.loadFile('./dist/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors')
