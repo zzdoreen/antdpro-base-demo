70b762d50fb92631e184cebb5009838652b0eadb
zzdoreen <zz1061831341@qq.com>
2024-04-08 15:33:15 +0800

    feat：build

--- dist/p__Less.js
+++ dist/p__Less.js
@@ -1,4 +1,4 @@
-(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[156],{66553:function(c,t,u){"use strict";u.r(t),u.d(t,{default:function(){return s}});var d=u(12215),n=u(85893),o=[{name:"\u53D8\u91CF\u590D\u7528",code:`
+(self.webpackChunkant_design_pro=self.webpackChunkant_design_pro||[]).push([[156],{66553:function(h,s,e){"use strict";e.r(s),e.d(s,{default:function(){return c}});var x=e(7359),u=e(27279),o=e(12215),d=e(53961),i=e(99802),n=e(85893),a=[{name:"\u53D8\u91CF\u590D\u7528",code:`
             {
                 @color: rgb(199, 138, 170);
                 @first-content: content;
@@ -19,8 +19,7 @@
                 width: 200px;
                 height: 200px;
             }
-        }
-        `},{name:" mixin \u51CF\u5C11\u91CD\u590D\u6837\u5F0F\u7F16\u5199",code:`
+        }`},{name:" mixin \u51CF\u5C11\u91CD\u590D\u6837\u5F0F\u7F16\u5199",code:`
         .borderStyle() {
             // \u5E26\u62EC\u53F7\u4E0D\u4F1A\u8F93\u51FA\u8BE5\u6837\u5F0F\uFF0C\u4E0D\u5E26\u7684\u8BDD\u4F1A\u7559\u5728css\u6837\u5F0F\u4E2D
             background-color: lightgreen;
@@ -42,8 +41,7 @@
                 #boxSize(150px);
                 .box-shadow;
             }
-        }
-        `},{name:"\u6620\u5C04",code:`
+        }`},{name:"\u6620\u5C04",code:`
         {
             @backgroundColor: {
                 primary: green;
@@ -55,5 +53,4 @@
                 width: 100px;
                 height: $width;
             }
-        }
-        `}];function s(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("div",{className:"title",children:"Less\uFF08Leaner Style Sheets \u7684\u7F29\u5199\uFF09\u662F\u4E00\u79CD\u5411\u540E\u517C\u5BB9\u7684 CSS \u8BED\u8A00\u6269\u5C55\u3002Less.js \u662F\u5C06 Less \u6837\u5F0F\u8F6C\u6362\u4E3A CSS \u6837\u5F0F\u7684 JavaScript \u5DE5\u5177\u3002"}),(0,n.jsx)("div",{className:"container",children:o.map(function(r){var e=r.name;return(0,n.jsx)("div",{className:"item",children:(0,n.jsxs)(d.xC,{style:{padding:30},color:["#ddd","#ccc"],children:[(0,n.jsx)("h1",{children:e}),(0,n.jsx)("div",{className:"content"+(e===" :extend \u590D\u7528\u6837\u5F0F(\u7EE7\u627F)"?" border-style":"")})]})},e)})}),(0,n.jsx)("div",{className:"container",children:o.map(function(r){var e=r.code,i=r.name;return(0,n.jsx)("div",{className:"item",children:(0,n.jsx)(d.xC,{style:{padding:20},color:["#ddd","#ccc"],reverse:!0,children:(0,n.jsx)("pre",{style:{textWrap:"wrap"},children:(0,n.jsx)("code",{children:e})})})},i)})})]})}}}]);
+        }`}];function c(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("div",{className:"title",children:"Less\uFF08Leaner Style Sheets \u7684\u7F29\u5199\uFF09\u662F\u4E00\u79CD\u5411\u540E\u517C\u5BB9\u7684 CSS \u8BED\u8A00\u6269\u5C55\u3002Less.js \u662F\u5C06 Less \u6837\u5F0F\u8F6C\u6362\u4E3A CSS \u6837\u5F0F\u7684 JavaScript \u5DE5\u5177\u3002"}),(0,n.jsx)("div",{className:"container",children:a.map(function(r){var t=r.name,l=r.code;return(0,n.jsxs)("div",{className:"item",children:[(0,n.jsxs)(o.LG,{style:{padding:30,height:400},color:["#ddd","#ccc"],children:[(0,n.jsx)("h1",{children:t}),(0,n.jsx)("div",{className:"content"+(t===" :extend \u590D\u7528\u6837\u5F0F(\u7EE7\u627F)"?" border-style":"")})]}),(0,n.jsx)(u.Z,{children:(0,n.jsx)(u.Z.Panel,{header:"code",children:(0,n.jsx)(d.Z,{language:"less",style:i.Z,children:l})},t)})]},t)})})]})}}}]);
